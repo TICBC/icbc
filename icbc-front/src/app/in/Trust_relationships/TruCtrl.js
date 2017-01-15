57ae9f3825582a9336097aa2ed4399ee09ccf43e
zilaijuan <1823663710@qq.com>
2017-01-15 15:42:49 +0800

    修改首页切换标签样式

--- icbc-front/src/app/in/Trust_relationships/TruCtrl.js
+++ icbc-front/src/app/in/Trust_relationships/TruCtrl.js
@@ -37,81 +37,142 @@ export default ($scope, $rootScope, qService, customerInfoRes, ToasterTool, resu
 	//////  画图
 	$(function(){
 		
-		var dom = document.getElementById("container");
+        // 路径配置
+       
+        
+        // 使用
+        
+            var option = {
+					title : {
+						text: '人物关系：乔布斯',
+						subtext: '数据来自人立方',
+						x:'right',
+						y:'bottom'
+					},
+					tooltip : {
+						trigger: 'item',
+						formatter: '{a} : {b}'
+					},
+					toolbox: {
+						show : true,
+						feature : {
+							restore : {show: true},
+							magicType: {show: true, type: ['force', 'chord']},
+							saveAsImage : {show: true}
+						}
+					},
+					legend: {
+						x: 'left',
+						data:['家人','朋友']
+					},
+					series : [
+						{
+							type:'force',
+							name : "人物关系",
+							ribbonType: false,
+							categories : [
+								{
+									name: '人物'
+								},
+								{
+									name: '家人'
+								},
+								{
+									name:'朋友'
+								}
+							],
+							itemStyle: {
+								normal: {
+									label: {
+										show: true,
+										textStyle: {
+											color: '#333'
+										}
+									},
+									nodeStyle : {
+										brushType : 'both',
+										borderColor : 'rgba(255,215,0,0.4)',
+										borderWidth : 1
+									},
+									linkStyle: {
+										type: 'curve'
+									}
+								},
+								emphasis: {
+									label: {
+										show: false
+										// textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
+									},
+									nodeStyle : {
+										//r: 30
+									},
+									linkStyle : {}
+								}
+							},
+							useWorker: false,
+							minRadius : 15,
+							maxRadius : 25,
+							gravity: 1.1,
+							scaling: 1.1,
+							roam: 'move',
+							nodes:[
+								{category:0, name: '乔布斯', value : 10, label: '乔布斯\n（主要）'},
+								{category:1, name: '丽萨-乔布斯',value : 2},
+								{category:1, name: '保罗-乔布斯',value : 3},
+								{category:1, name: '克拉拉-乔布斯',value : 3},
+								{category:1, name: '劳伦-鲍威尔',value : 7},
+								{category:2, name: '史蒂夫-沃兹尼艾克',value : 5},
+								{category:2, name: '奥巴马',value : 8},
+								{category:2, name: '比尔-盖茨',value : 9},
+								{category:2, name: '乔纳森-艾夫',value : 4},
+								{category:2, name: '蒂姆-库克',value : 4},
+								{category:2, name: '龙-韦恩',value : 1},
+							],
+							links : [
+								{source : '丽萨-乔布斯', target : '乔布斯', weight : 1, name: '女儿'},
+								{source : '保罗-乔布斯', target : '乔布斯', weight : 2, name: '父亲'},
+								{source : '克拉拉-乔布斯', target : '乔布斯', weight : 1, name: '母亲'},
+								{source : '劳伦-鲍威尔', target : '乔布斯', weight : 2},
+								{source : '史蒂夫-沃兹尼艾克', target : '乔布斯', weight : 3, name: '合伙人'},
+								{source : '奥巴马', target : '乔布斯', weight : 1},
+								{source : '比尔-盖茨', target : '乔布斯', weight : 6, name: '竞争对手'},
+								{source : '乔纳森-艾夫', target : '乔布斯', weight : 1, name: '爱将'},
+								{source : '蒂姆-库克', target : '乔布斯', weight : 1},
+								{source : '龙-韦恩', target : '乔布斯', weight : 1},
+								{source : '克拉拉-乔布斯', target : '保罗-乔布斯', weight : 1},
+								{source : '奥巴马', target : '保罗-乔布斯', weight : 1},
+								{source : '奥巴马', target : '克拉拉-乔布斯', weight : 1},
+								{source : '奥巴马', target : '劳伦-鲍威尔', weight : 1},
+								{source : '奥巴马', target : '史蒂夫-沃兹尼艾克', weight : 1},
+								{source : '比尔-盖茨', target : '奥巴马', weight : 6},
+								{source : '比尔-盖茨', target : '克拉拉-乔布斯', weight : 1},
+								{source : '蒂姆-库克', target : '奥巴马', weight : 1}
+							]
+						}
+					]
+				};
+				var ecConfig = require('echarts.baidu.com/echarts/config');
+				function focus(param) {
+					var data = param.data;
+					var links = option.series[0].links;
+					var nodes = option.series[0].nodes;
+					if (
+						data.source !== undefined
+						&& data.target !== undefined
+					) { //点击的是边
+						var sourceNode = nodes.filter(function (n) {return n.name == data.source})[0];
+						var targetNode = nodes.filter(function (n) {return n.name == data.target})[0];
+						console.log("选中了边 " + sourceNode.name + ' -> ' + targetNode.name + ' (' + data.weight + ')');
+					} else { // 点击的是点
+						console.log("选中了" + data.name + '(' + data.value + ')');
+					}
+				}
+				myChart.on(ecConfig.EVENT.CLICK, focus)
 
-        var myChart1 = echarts.init(dom);
-
-        var app = {};
-
-        var option = null;
-	
-        myChart1.showLoading();
-
-        $.get('data/data.gexf', function (xml) {
-			
-            myChart1.hideLoading();
-
-            var graph = echarts.dataTool.gexf.parse(xml);
-			
-            var categories = [];
-            for (var i = 0; i < 9; i++) {
-                categories[i] = {
-                    name: '用户' + i
-                };
-            }
-            graph.nodes.forEach(function (node) {
-                node.itemStyle = null;
-                node.value = node.symbolSize;
-                node.label = {
-                    normal: {
-                        show: node.symbolSize > 30
-                    }
-                };
-                node.category = node.attributes.modularity_class;
-            });
-			console.log(graph);
-            option = {
-                title: {
-                    text: '当前用户风险关系信息',
-                    subtext: 'Default layout',
-                    top: 'bottom',
-                    left: 'center'
-                },
-                tooltip: {},
-                legend: [{
-                    // selectedMode: 'single',
-                    data: categories.map(function (a) {
-                        return a.name;
-                    })
-                }],
-                animationDuration: 1500,
-                animationEasingUpdate: 'quinticInOut',
-                series : [
-                    {
-                        name: '当前用户风险关系信息',
-                        type: 'graph',
-                        layout: 'none',
-                        data: graph.nodes,
-                        links: graph.links,
-                        categories: categories,
-                        roam: true,
-                        label: {
-                            normal: {
-                                position: 'right',
-                                formatter: '{b}'
-                            }
-                        },
-                        lineStyle: {
-                            normal: {
-                                curveness: 0.4
-                            }
-                        }
-                    }
-                ]
-            };
-
-            myChart1.setOption(option);
-        }, 'xml');
+				myChart.on(ecConfig.EVENT.FORCE_LAYOUT_END, function () {
+					console.log(myChart.chart.force.getPosition());
+				});
+            
 	});
 	
 };
